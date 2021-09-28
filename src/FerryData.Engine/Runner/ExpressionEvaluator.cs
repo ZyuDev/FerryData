04724a3e3664e51aac2ce1ea0c4471e2ebc91e6d
MikhaZyu <MikhaZyu@gmail.com>
2021-09-28 22:11:13 +0300

    Передача параметров между шагами

---
+++ src/FerryData.Engine/Runner/ExpressionEvaluator.cs
@@ -0,0 +1,68 @@
+﻿using DynamicExpresso;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerryData.Engine.Runner
+{
+    public class ExpressionEvaluator
+    {
+        private Interpreter _interpreter;
+        private Logger _logger;
+
+        public ExpressionEvaluator(Dictionary<string, object> context, Logger logger)
+        {
+            _interpreter = new Interpreter(InterpreterOptions.DefaultCaseInsensitive);
+            _logger = logger;
+
+            foreach (var kvp in context)
+            {
+                var varName = kvp.Key;
+                var val = kvp.Value;
+                _interpreter.SetVariable(varName, val);
+
+            }
+        }
+
+        public object Eval(string expression)
+        {
+            object result = null;
+
+            try
+            {
+                result = _interpreter.Eval(expression);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Cannot eval Formula: {0}. Message: {1}",
+                    expression,
+                    e.Message);
+
+            }
+
+            return result;
+        }
+
+        public T Eval<T>(string expression)
+        {
+            T result = default(T);
+
+            try
+            {
+                result = _interpreter.Eval<T>(expression);
+            }
+            catch (Exception e)
+            {
+                _logger.Error("Cannot eval Formula: {0}. Message: {1}",
+                    expression,
+                    e.Message);
+
+            }
+
+            return result;
+        }
+    }
+}
