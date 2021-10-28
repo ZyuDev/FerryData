b56e7ef0ddd9dad70a70ae1dfd93223dc2a1132e
MikhaZyu <MikhaZyu@gmail.com>
2021-10-28 14:32:13 +0300

    send to rabbit command

---
+++ src/FerryData.Engine/Runner/Commands/WorkflowSendToRabbitCommand.cs
@@ -0,0 +1,36 @@
+﻿using FerryData.Engine.Abstract;
+using FerryData.Engine.Models;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerryData.Engine.Runner.Commands
+{
+    public class WorkflowSendToRabbitCommand: IWorkflowCommand
+    {
+        private readonly WorkflowHttpAction _settings;
+        private readonly Logger _logger;
+        private Dictionary<string, object> _stepsData;
+
+        public WorkflowSendToRabbitCommand(WorkflowHttpAction settings, Dictionary<string, object> stepsData, Logger logger)
+        {
+            _settings = settings;
+            _stepsData = stepsData;
+            _logger = logger;
+        }
+
+        public async Task<IWorkflowCommandResult> ExecuteAsync()
+        {
+            //TODO: Send to rabbit here
+
+            var execResult = new WorkflowStepExecuteResult();
+
+            _logger.Info($"Step send to Rabbit");
+
+            return execResult;
+        }
+    }
+}
